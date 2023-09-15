f37f9dfa62a8fd6c6623fb071f7a11b19aac0f67
Jack smith Jaimes Rua <123602839+xzjackxz2003@users.noreply.github.com>
2023-09-15 18:22:26 -0500

    Add files via upload

--- BancodeCredito/src/bancodecredito/BancodeCredito.java
+++ BancodeCredito/src/bancodecredito/BancodeCredito.java
@@ -21,6 +21,7 @@ public class BancodeCredito {
         final int saldo_actual = 200;
         String optionSeleccionada=" ";
         String monto_Deposito=" ";
+        String monto_Retirar="";
         double Ver_saldo;
         double Depositar;
         double Retirar;
@@ -41,18 +42,41 @@ public class BancodeCredito {
         }
         
         Scanner cs = new Scanner(System.in);
-        monto_Deposito = cs.nextLine();
-        if (optionSeleccionada.equals("2")){
-            System.out.println("Cuanto vas a depositar: ");
-            System.out.println("Saldo actual: ");
-            System.out.println(saldo_actual + monto_Deposito);
+        System.out.println("Cuanto vas a depositar: ");
+        
+        
+        if  (optionSeleccionada.equals("2")){
+            System.out.println("Saldo_actual: " + saldo_actual);
+            System.out.println("monto_Deposito: ");
+            
+            monto_Deposito = cs.nextLine();
+            
+            int cantidadDeposito = Integer.parseInt(monto_Deposito);
+            
+            int nuevoSaldo = saldo_actual + cantidadDeposito;
+            System.out.println("Nuevo saldo: " + nuevoSaldo);
+            
+        }
+        
+        Scanner rt = new Scanner(System.in);
+        System.out.println("Cuanto vas a retirar: ");
+         
+        
+        
+        if (optionSeleccionada.equals("3")){
+        System.out.println("Saldo_actual: " + saldo_actual);
+        System.out.println("Monto a retirar: ");
+        
+        monto_Retirar = rt.nextLine();
+        
+        int cantidadRetiro = Integer.parseInt(monto_Retirar);
+        
+        int nuevoRetiro = saldo_actual - cantidadRetiro;
+        
+        System.out.println("Nuevo retiro: " + nuevoRetiro);
+        
         }
         
         
-        
-        
-        
-       
     }
-    
 }
