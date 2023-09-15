d2b32ad5adf23a81b39b5a3f99ba996e1fd85db7
Jack smith Jaimes Rua <123602839+xzjackxz2003@users.noreply.github.com>
2023-09-14 20:35:27 -0500

    Add files via upload

---
+++ BancodeCredito/src/bancodecredito/BancodeCredito.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bancodecredito;
+
+
+/**
+ *
+ * @author jackypiero
+ */
+public class BancodeCredito {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        final int saldo_inicial = 200;
+        int option0;
+        int option1;
+        int option2;
+        double Ver_saldo;
+        double Depositar;
+        double Retirar;
+        
+        System.out.println("***BANCO DE CREDITO JACK***");
+        System.out.println("1. VER SALDO: ");
+        System.out.println("2. DEPOSITAR: ");
+        System.out.println("3. RETIRAR: ");
+        
+        if (option1 == Ver_saldo){
+            System.out.println("Su sald inicial es:");
+        } else if(option2 == Depositar){
+           System.out.println("Ingresa el monto a despositar:");
+        }
+        
+        
+        
+        
+       
+    }
+    
+}
