ab47b588a1098d3269b13ec9df639a9c6d4e07e0
Jack smith Jaimes Rua <123602839+xzjackxz2003@users.noreply.github.com>
2023-09-15 16:28:44 -0500

    Add files via upload

--- BancodeCredito/src/bancodecredito/BancodeCredito.java
+++ BancodeCredito/src/bancodecredito/BancodeCredito.java
@@ -18,8 +18,9 @@ public class BancodeCredito {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        final int saldo_inicial = 200;
+        final int saldo_actual = 200;
         String optionSeleccionada=" ";
+        String monto_Deposito=" ";
         double Ver_saldo;
         double Depositar;
         double Retirar;
@@ -31,12 +32,22 @@ public class BancodeCredito {
         
         
         Scanner sc = new Scanner(System.in);
+        
         System.out.println("Introduzca la opcion:");
         optionSeleccionada = sc.nextLine();        
         
         if (optionSeleccionada.equals("1")){
-            System.out.println("Su saldo es: " + saldo_inicial);
+            System.out.println("Su saldo es: " + saldo_actual);
         }
+        
+        Scanner cs = new Scanner(System.in);
+        monto_Deposito = cs.nextLine();
+        if (optionSeleccionada.equals("2")){
+            System.out.println("Cuanto vas a depositar: ");
+            System.out.println("Saldo actual: ");
+            System.out.println(saldo_actual + monto_Deposito);
+        }
+        
         
         
         
